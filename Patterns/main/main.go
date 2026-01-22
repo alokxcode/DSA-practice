759f2e48e578c96153a6e680e6e5f2663caf6f0b
Alok <codexmanavya@gmail.com>
2026-01-22 11:38:48 +0530

    solved patterns

---
+++ Patterns/main/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+
+	pattern "github.com/alokxcode/dsa/Patterns/patterns"
+)
+
+func main() {
+	fmt.Println("\nFirst Pattern")
+	pattern.One()
+
+	fmt.Println("\nSecond Pattern")
+	pattern.Two()
+
+	fmt.Println("\nThird Pattern")
+	pattern.Three()
+
+	fmt.Println("\nFourth Pattern")
+	pattern.Four()
+
+	fmt.Println("\nFifth Pattern")
+	pattern.Five()
+
+	fmt.Println("\nSixth Pattern")
+	pattern.Six()
+
+	fmt.Println("\nSeventh Pattern")
+	pattern.Seven()
+
+	fmt.Println("\nEighth Pattern")
+	pattern.Eight()
+
+	fmt.Println("\nNinth Pattern")
+	pattern.Nine()
+}
