7e6c88d25e2b70b1daf28bf7d6753f15450f3196
Alok <codexmanavya@gmail.com>
2026-01-29 08:14:02 +0530

    binary search

--- Algorithms/main/main.go
+++ Algorithms/main/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	algorithms "github.com/alokxcode/dsa/Algorithms"
+)
+
+func main() {
+	nums := []int{4, 6, 7, 7, 8, 11, 40, 43, 45, 65, 70, 70, 77}
+	target := 43
+
+	result := algorithms.Linear_Search(nums, target)
+	fmt.Println(result)
+
+	result2 := algorithms.Binary_Search(nums, target)
+	fmt.Println(result2)
 }
